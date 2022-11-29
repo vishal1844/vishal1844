d83e062131b5756215cd95a068b46ce5495795fa
vishal1844 <104387858+vishal1844@users.noreply.github.com>
2022-11-29 23:58:24 +0530

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std; 
+#define pb push_back 
+#define ss second
+#define ppb pop_back
+#define ins insert
+#define lb lower_bound
+#define ub upper_bound 
+#define umap unordered_map
+#define rep(i,n) for(int i = 0; i < (int)(n); i++)
+#define rrep(i,n) for(int i = (int)(n) - 1; i >= 0; i--)
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+template<class T> bool chmax(T& a, const T& b) { if (a < b) { a = b; return true; } else return false; }
+template<class T> bool chmin(T& a, const T& b) { if (b < a) { a = b; return true; } else return false; }
+using ll = long long;
+using p = pair<int,int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>; 
+using vvl = vector<vl>;  
+void solve()
+{
+   
+} 
+int main()
+{
+   int t;
+   cin>>t;
+   for(int i=0;i<t;++i)
+{ 
+     solve();
+}
+}
